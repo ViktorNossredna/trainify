d2acf5fad4cb8ac2cbdcae8535380c535a1bc4f4
Viktor Andersson <viktor.nossredna@gmail.com>
2020-01-02 13:27:46 +0100

    Initial commit with dummy cloud function

---
+++ main.py
@@ -0,0 +1,39 @@
+import datetime
+from google.cloud import datastore
+
+datastore_client = datastore.Client()
+message_kind = 'bajs'
+
+def root(request):
+    """Responds to any HTTP request.
+    Args:
+        request (flask.Request): HTTP request object.
+    Returns:
+        The response text or any set of values that can be turned into a
+        Response object using
+        `make_response <http://flask.pocoo.org/docs/1.0/api/#flask.Flask.make_response>`.
+    """
+    request_json = request.get_json()
+
+    if request.args and 'message' in request.args:
+        message = request.args.get('message')
+        store_message(message)
+        return message
+    elif request_json and 'message' in request_json:
+        message = request_json['message']
+        store_message(message)
+        return message
+    else:
+        message = 'Pappa bajsar en stor bajs!'
+        store_message(message)
+        return message
+
+def store_message(message):
+    print(f'storing message: {message} to datastore with kind {message_kind}')
+    entity = datastore.Entity(key=datastore_client.key(message_kind))
+    entity.update({
+        'timestamp': datetime.datetime.utcnow(),
+        'message': message
+    })
+
+    datastore_client.put(entity)
